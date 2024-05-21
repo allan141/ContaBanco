01a0e652aa6841720ff910754ac26f3906633b96
allan141 <allancavalcante141@gmail.com>
2024-05-21 13:29:35 -0300

    UpdateCode
    Atualização do Código.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,16 +1,34 @@
-public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        double saldo = 17;
-        double valorSolicitado = 18;
+import java.util.Scanner;
 
-        if(saldo > valorSolicitado){
-            saldo = saldo - valorSolicitado;
-            System.out.println("Saque Efetuado");
-            System.out.println("Seu novo saldo da conta é: R$" + saldo);
-        }
-        else { 
-            System.out.println("Saldo Insulficiente");
-        }
+/**
+ * ContaTerminal
+ */
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int conta;
+        System.out.println("Digite o seu Número da Conta: " );
+        conta = ler.nextInt();
+
+        Scanner ler2 = new Scanner(System.in);
+        String agencia;
+        System.out.println("Digite a sua Agência: ");
+        agencia = ler2.next();
+
+        Scanner ler3 = new Scanner(System.in);
+        String nomeCliente;
+        System.out.println("Digite Seu Nome: ");
+        nomeCliente = ler3.next();
+
+        Scanner ler4 = new Scanner(System.in);
+        double saldo;
+        System.out.println("Digite qual é o valor do seu Depósito ");
+        saldo = ler4.nextDouble();
+
+        System.out.println("Olá, " + nomeCliente + ", Obrigado por criar uma conta em nosso Banco.");
+        System.out.println("Sua Agência é " + agencia + ", Conta " + conta);
+        System.out.println("E seu saldo R$" + saldo + " Já está Disponivel para Saque.");
 
     }
 }
